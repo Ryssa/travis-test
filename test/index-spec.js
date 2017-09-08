6ac6b4c96226b2a6f923a96f6d98f09eaf9c1106
larissa <lari.barabasz@gmail.com>
2017-09-08 01:17:01 -0300

    bleh

---
+++ test/index-spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('yeah', function() {
+    describe('yeah test', function() {
+        it('true is true', function () {
+            assert.equal(true, true);
+        });
+    });
+});
